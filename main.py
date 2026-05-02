3c4ed532c1b2a0608a23f5f3cd4994a3058facf2
Matheus Andrade <matheusper@ufu.br>
2026-05-02 15:24:52 -0300

    ✨ feat: add image output and result saving
    - 🖼️ integrated OpenCV to save prediction results
    - 💾 added  to export annotated images as 'resultado.jpg'
    - 📢 added success message print for better user feedback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import supervision as sv
+import cv2
 from rfdetr import RFDETRSegMedium
 from rfdetr.assets.coco_classes import COCO_CLASSES
 
@@ -10,3 +11,6 @@ labels = [f"{COCO_CLASSES[class_id]}" for class_id in detections.class_id]
 
 annotated_image = sv.MaskAnnotator().annotate(detections.metadata["source_image"], detections)
 annotated_image = sv.LabelAnnotator().annotate(annotated_image, detections, labels)
+
+cv2.imwrite("resultado.jpg", annotated_image)
+print("Imagem salva com sucesso como resultado.jpg!")
